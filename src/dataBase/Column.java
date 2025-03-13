7448b34a5b4fa8c92d7356d49d63b3895f2b2827
Wannes MM <wannesmm@gmail.com>
2025-03-13 14:53:45 +0100

    Documentation

--- src/dataBase/Column.java
+++ src/dataBase/Column.java
@@ -1,49 +1,90 @@
-
 package database;
 
 import java.util.ArrayList;
 
+/**
+ * Represents a column in a database table, containing multiple cells and having
+ * a specific data type.
+ */
 public class Column {
     private ColumnType type;
     private boolean allowBlank = false;
     private ArrayList<Cell> cells;
     private String defaultValue;
 
+    /**
+     * Constructs a new Column with the default type of STRING and initializes
+     * cells.
+     */
     public Column() {
         this.type = ColumnType.STRING;
         resetDefaultValue();
         this.cells = new ArrayList<>();
     }
 
+    /**
+     * Gets the type of data stored in this column.
+     *
+     * @return the current ColumnType of this column.
+     */
     public ColumnType getType() {
         return type;
     }
 
+    /**
+     * Registers a cell to this column.
+     *
+     * @param cell the Cell object to be added to the column.
+     */
     public void registerCell(Cell cell) {
         this.cells.add(cell);
     }
 
+    /**
+     * Checks whether blank values are allowed in this column.
+     *
+     * @return true if blank values are allowed, false otherwise.
+     */
     public boolean getAllowBlank() {
         return allowBlank;
     }
 
+    /**
+     * Changes the data type of the column and updates existing cells accordingly.
+     *
+     * @param type the new ColumnType to set.
+     */
     public void editColumnType(ColumnType type) {
         this.type = type;
         resetDefaultValue();
         for (Cell cell : this.cells) {
-            cell.setValue(cell.getValue());
+            cell.setValue(cell.getValue()); // Ensures cells conform to the new type
         }
     }
 
+    /**
+     * Sets whether blank values are allowed in this column.
+     *
+     * @param allowBlank true to allow blank values, false otherwise.
+     */
     public void setAllowBlank(boolean allowBlank) {
         this.allowBlank = allowBlank;
         resetDefaultValue();
     }
 
+    /**
+     * Retrieves all cells stored in this column.
+     *
+     * @return an ArrayList of Cell objects contained in the column.
+     */
     public ArrayList<Cell> getCells() {
         return cells;
     }
 
+    /**
+     * Resets the default value of the column based on its type and whether blank
+     * values are allowed.
+     */
     public void resetDefaultValue() {
         if (getAllowBlank()) {
             defaultValue = null;
@@ -65,14 +106,28 @@ public class Column {
         }
     }
 
+    /**
+     * Sets the default value for this column.
+     *
+     * @param defaultValue the default value to be set.
+     */
     public void setDefaultValue(String defaultValue) {
         this.defaultValue = defaultValue;
     }
 
+    /**
+     * Retrieves the default value of this column.
+     *
+     * @return the current default value as a String.
+     */
     public String getDefaultValue() {
         return defaultValue;
     }
 
+    /**
+     * Cycles through the different column types in the order: STRING -> INTEGER ->
+     * BOOLEAN -> EMAIL -> STRING.
+     */
     public void toggleColumnType() {
         if (type == ColumnType.STRING) {
             editColumnType(ColumnType.INTEGER);
--- src/dataBase/dataBase.java
+++ src/dataBase/dataBase.java
@@ -5,15 +5,25 @@ import java.util.HashMap;
 import java.util.Set;
 import java.util.ArrayList;
 
+/**
+ * Represents a simple database system that manages tables and their columns and
+ * rows.
+ */
 public class dataBase {
     private Map<String, Table> tables;
 
+    /**
+     * Constructs a new database with an empty table collection.
+     */
     public dataBase() {
         tables = new HashMap<String, Table>();
     }
 
     private int tableCounter = 1;
 
+    /**
+     * Creates a new table with a unique name and adds it to the database.
+     */
     public void createTable() {
         String tableName = "Table" + tableCounter++;
         while (tables.containsKey(tableName)) {
@@ -22,16 +32,32 @@ public class dataBase {
         tables.put(tableName, new Table());
     }
 
+    /**
+     * Retrieves the set of table names in the database.
+     * 
+     * @return a set of table names.
+     */
     public Set<String> getTables() {
         return tables.keySet();
     }
 
-    // Methods for the current table
-
+    /**
+     * Deletes a table from the database.
+     * 
+     * @param tableName the name of the table to delete.
+     */
     public void deleteTable(String tableName) {
         tables.remove(tableName);
     }
 
+    /**
+     * Renames an existing table in the database.
+     *
+     * @param oldName the current name of the table.
+     * @param newName the new name for the table.
+     * @throws Error if the old table name does not exist or the new name already
+     *               exists.
+     */
     public void editTableName(String oldName, String newName) {
         if (!tables.containsKey(oldName))
             throw new Error("Table does not exist");
@@ -44,20 +70,49 @@ public class dataBase {
         tables.remove(oldName);
     }
 
+    /**
+     * Checks if a table name is valid.
+     * 
+     * @param tableName the name of the table to check.
+     * @return true if the table name is valid, false otherwise.
+     */
     public boolean isValidTableName(String tableName) {
         return !tables.containsKey(tableName);
     }
 
+    /**
+     * Checks if a column name is valid for a table.
+     * 
+     * @param tableName  the name of the table to check.
+     * @param columnName the name of the column to check.
+     * @return true if the column name is valid, false otherwise.
+     */
     public boolean isValidColumnName(String tableName, String columnName) {
         return !tables.get(tableName).getColumns().contains(columnName);
     }
 
+    /**
+     * Checks if a column allows blank values.
+     * 
+     * @param tableName  the name of the table to check.
+     * @param columnName the name of the column to check.
+     * @return true if the column allows blank values, false otherwise.
+     */
     public boolean columnAllowBlank(String tableName, String columnName) {
         return tables.get(tableName).columnAllowBlank(columnName);
     }
 
+    /**
+     * Generates an incremental number for a new column name.
+     */
     private int columnCounter = 1;
 
+    /**
+     * Adds a new column to a table.
+     * 
+     * @param tableName the name of the table to add the column to.
+     * @throws Error if the table does not exist.
+     */
     public void addColumn(String tableName) {
         if (!tables.containsKey(tableName))
             throw new Error("Table does not exist");
@@ -65,62 +120,167 @@ public class dataBase {
         tables.get(tableName).createColumn(columnName);
     }
 
+    /**
+     * Adds a new row to a table.
+     * 
+     * @param tableName the name of the table to add the row to.
+     * @throws Error if the table does not exist.
+     */
     public void addRow(String tableName) {
         tables.get(tableName).createRow();
     }
 
+    /**
+     * Deletes a row from a table.
+     * 
+     * @param tableName the name of the table to delete the row from.
+     * @param index     the index of the row to delete.
+     * @throws Error if the table does not exist.
+     */
     public void deleteRow(String tableName, int index) {
         tables.get(tableName).deleteRow(index);
     }
 
+    /**
+     * Deletes a column from a table.
+     * 
+     * @param tableName  the name of the table to delete the column from.
+     * @param columnName the name of the column to delete.
+     * @throws Error if the table does not exist.
+     */
     public void deleteColumn(String tableName, String columnName) {
         tables.get(tableName).deleteColumn(columnName);
     }
 
+    /**
+     * Edits a cell in a table.
+     * 
+     * @param tableName  the name of the table containing the cell.
+     * @param columnName the name of the column containing the cell.
+     * @param rowIndex   the index of the row containing the cell.
+     * @param value      the new value for the cell.
+     */
     public void editCell(String tableName, String columnName, int rowIndex, String value) {
         tables.get(tableName).editCell(columnName, rowIndex, value);
     }
 
+    /**
+     * Retrieves the value of a cell in a table.
+     * 
+     * @param tableName  the name of the table containing the cell.
+     * @param columnName the name of the column containing the cell.
+     * @param rowIndex   the index of the row containing the cell.
+     * @return the value of the cell.
+     */
     public Object getCell(String tableName, String columnName, int rowIndex) {
         return tables.get(tableName).getCell(columnName, rowIndex).getValue();
     }
 
+    /**
+     * Checks if a cell in a table is valid.
+     * 
+     * @param tableName  the name of the table containing the cell.
+     * @param columnName the name of the column containing the cell.
+     * @param rowIndex   the index of the row containing the cell.
+     * @return true if the cell is valid, false otherwise.
+     */
     public boolean isCellValid(String tableName, String columnName, int rowIndex) {
         return tables.get(tableName).getCell(columnName, rowIndex).isValid();
     }
 
+    /**
+     * Retrieves the column names of a table.
+     * 
+     * @param tableName the name of the table to retrieve the column names from.
+     * @return an ArrayList of column names.
+     */
     public ArrayList<String> getColumnNames(String tableName) {
         return tables.get(tableName).getColumns();
     }
 
+    /**
+     * Retrieves the rows of a table.
+     * 
+     * @param tableName the name of the table to retrieve the rows from.
+     * @return an ArrayList of rows.
+     */
     public ArrayList<ArrayList<String>> getRows(String tableName) {
         return tables.get(tableName).getRows();
     }
 
+    /**
+     * Retrieves a row from a table.
+     * 
+     * @param tableName the name of the table to retrieve the row from.
+     * @param index     the index of the row to retrieve.
+     * @return an ArrayList of row values.
+     */
     public ArrayList<String> getRow(String tableName, int index) {
         return tables.get(tableName).getRow(index);
     }
 
+    /**
+     * Retrieves the type of a column in a table.
+     * 
+     * @param tableName  the name of the table containing the column.
+     * @param columnName the name of the column to retrieve the type of.
+     * @return the type of the column.
+     */
     public void editColumnType(String tableName, String columnName, ColumnType type) {
         tables.get(tableName).editColumnType(columnName, type);
     }
 
+    /**
+     * Retrieves the default value of a column in a table.
+     * 
+     * @param tableName  the name of the table containing the column.
+     * @param columnName the name of the column to retrieve the default value of.
+     * @return the default value of the column.
+     */
     public void editColumnName(String tableName, String oldName, String newName) {
         tables.get(tableName).editColumnName(oldName, newName);
     }
 
+    /**
+     * Retrieves the default value of a column in a table.
+     * 
+     * @param tableName  the name of the table containing the column.
+     * @param columnName the name of the column to retrieve the default value of.
+     * @return the default value of the column.
+     */
     public ColumnType getColumnType(String tableName, String columnName) {
         return tables.get(tableName).getColumnType(columnName);
     }
 
+    /**
+     * Retrieves the default value of a column in a table.
+     * 
+     * @param tableName  the name of the table containing the column.
+     * @param columnName the name of the column to retrieve the default value of.
+     * @return the default value of the column.
+     */
     public String getDefaultColumnValue(String tableName, String columnName) {
         return tables.get(tableName).getDefaultColumnValue(columnName);
     }
 
+    /**
+     * Retrieves the default value of a column in a table.
+     * 
+     * @param tableName  the name of the table containing the column.
+     * @param columnName the name of the column to retrieve the default value of.
+     * @return the default value of the column.
+     */
     public void editDefaultColumnValue(String tableName, String columnName, String value) {
         tables.get(tableName).editDefaultColumnValue(columnName, value);
     }
 
+    /**
+     * Retrieves the default value of a column in a table.
+     * 
+     * @param tableName  the name of the table containing the column.
+     * @param columnName the name of the column to retrieve the default value of.
+     * @return the default value of the column.
+     */
     public void toggleColumnType(String tableName, String columnName) {
         tables.get(tableName).toggleColumnType(columnName);
     }
