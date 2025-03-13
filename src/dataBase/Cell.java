ace2a3a584ea0a964857d9d2a704497c998c7238
Ety60 <66908980+Ety60@users.noreply.github.com>
2025-03-13 13:45:24 +0100

    added docs for row and cell

--- src/dataBase/Cell.java
+++ src/dataBase/Cell.java
@@ -1,23 +1,40 @@
 package database;
 
+/**
+ * Represents a cell in a table, associated with a specific column.
+ * A cell holds a value and checks if the value is valid according to the column type.
+ */
 public class Cell {
     private Column column;
     private String value = null;
     private boolean valid = true;
 
+    /**
+     * Constructs a Cell object associated with a given column.
+     * Initializes the cell's value to the default value based on the column type.
+     *
+     * @param column The column this cell belongs to.
+     */
     public Cell(Column column) {
         this.column = column;
         setDefault();
     }
 
-    // Set the value of a cell. The input is a string that will be converted to the
-    // correct column type. The method returns true when the value is set correctly.
-    // Otherwise false
+    /**
+     * Sets the value of the cell.
+     * The input is a string that will be validated according to the column type.
+     *
+     * @param value The value to be set for this cell.
+     */
     public void setValue(String value) {
         this.value = value;
         checkValid();
     }
 
+    /**
+     * Checks if the current value of the cell is valid according to the column type.
+     * Updates the 'valid' field based on the validation result.
+     */
     private void checkValid() {
         switch (column.getType()) {
             case INTEGER:
@@ -25,7 +42,6 @@ public class Cell {
                     Integer.parseInt(value);
                 } catch (NumberFormatException e) {
                     valid = false;
-                    // throw new Error("Value was not an integer, but column type is integer");
                 }
                 break;
 
@@ -35,18 +51,15 @@ public class Cell {
             case BOOLEAN:
                 switch (value.toUpperCase()) {
                     case "TRUE":
-                        valid = true;
-                        break;
                     case "FALSE":
                         valid = true;
                         break;
                     default:
                         valid = false;
-                        // throw new Error("Value was not a valid boolean, but column type is boolean");
                 }
                 break;
             case EMAIL:
-                if (value.contains("@")) {
+                if (value.contains("@") && value.contains(".")) {
                     valid = true;
                 } else {
                     valid = false;
@@ -57,20 +70,54 @@ public class Cell {
         }
     }
 
+    /**
+     * Returns whether the current value of the cell is valid.
+     *
+     * @return true if the value is valid, false otherwise.
+     */
     public boolean isValid() {
         return valid;
     }
 
-    // Set the value of a cell to the default value depending on wheter column
-    // allows blank values or not
+    /**
+     * Sets the value of the cell to the default value based on the column type.
+     * If the column allows blank values, the value is set to null.
+     */
     public void setDefault() {
-        value = column.getDefaultValue();
+        if (column.getAllowBlank()) {
+            this.value = null;
+            return;
+        }
+        switch (column.getType()) {
+            case INTEGER:
+                this.value = "0";
+                break;
+            case STRING:
+                this.value = "";
+                break;
+            case BOOLEAN:
+                this.value = "false";
+                break;
+            case EMAIL:
+                this.value = "@";
+                break;
+        }
     }
 
+    /**
+     * Returns the current value of the cell.
+     *
+     * @return The value of the cell as a string.
+     */
     public String getValue() {
         return value;
     }
 
+    /**
+     * Returns the column associated with this cell.
+     *
+     * @return The column this cell belongs to.
+     */
     public Column getColumn() {
         return column;
     }
