73dd5dca6307dfbb980530fff9aff1ab8a54bc89
Wannes MM <wannesmm@gmail.com>
2025-05-21 22:03:00 +0200

    TableFormsView complete

--- src/application/screens/TableFormsView.java
+++ src/application/screens/TableFormsView.java
@@ -2,30 +2,21 @@ package application.screens;
 
 import java.awt.event.KeyEvent;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.function.Consumer;
 import application.DatabaseAppContext;
 import application.widgets.Header;
-import application.widgets.ValueCell;
 import clutter.abstractwidgets.Widget;
-import clutter.core.ResizableGridController;
+import clutter.core.Orientation;
 import clutter.core.ScrollController;
 import clutter.decoratedwidgets.Text;
-import clutter.inputwidgets.CheckBox;
-import clutter.inputwidgets.Clickable;
-import clutter.inputwidgets.CycleButton;
 import clutter.inputwidgets.InputText;
-import clutter.layoutwidgets.Center;
 import clutter.layoutwidgets.Column;
-import clutter.layoutwidgets.ConstrainedBox;
+import clutter.layoutwidgets.Grid;
 import clutter.layoutwidgets.GrowToFit;
-import clutter.layoutwidgets.ResizableGrid;
+import clutter.layoutwidgets.Padding;
 import clutter.layoutwidgets.ScrollableView;
 import clutter.layoutwidgets.enums.Alignment;
-import database.ColumnType;
 import database.Database.TableDataChangeListener;
-import database.Database.TableDesignChangeListener;
 
 /**
  * A screen that represents the table design mode view.
@@ -71,17 +62,31 @@ public class TableFormsView extends DatabaseScreen implements TableDataChangeLis
         switch (id) {
             case KeyEvent.KEY_PRESSED:
                 switch (keyCode) {
-                    case KeyEvent.VK_DELETE:
+                    case KeyEvent.VK_PAGE_UP:
                         setState(() -> {
-                            for (String column : selectedColumns) {
-                                context.getDatabase().deleteColumn(tableName, column);
-                            }
-                            selectedColumns.clear();
+                            if (rowNumber < context.getDatabase().getRows(tableName).size())
+                                rowNumber++;
+                        });
+                        return true;
+                    case KeyEvent.VK_PAGE_DOWN:
+                        setState(() -> {
+                            if (rowNumber > 0)
+                                rowNumber--;
+                        });
+                        return true;
+                    case KeyEvent.VK_D:
+                        setState(() -> {
+                            if (rowNumber < context.getDatabase().getRows(tableName).size())
+                                context.getDatabase().deleteRow(tableName, rowNumber);
+                        });
+                        return true;
+                    case KeyEvent.VK_N:
+                        setState(() -> {
+                            context.getDatabase().addRow(tableName);
+                            rowNumber = context.getDatabase().getRows(tableName).size() - 1;
                         });
                         return true;
 
-                    case KeyEvent.VK_ESCAPE:
-                        return true;
 
                     // case KeyEvent.VK_ENTER: {
                     // onOpenRowsView.accept(tableName);
@@ -95,7 +100,7 @@ public class TableFormsView extends DatabaseScreen implements TableDataChangeLis
                 return false;
         }
     }
-    
+
     /**
      * Builds the table design mode view.
      * 
@@ -103,9 +108,10 @@ public class TableFormsView extends DatabaseScreen implements TableDataChangeLis
      */
     @Override
     public Widget build() {
-        return new Column(new Header(context, tableName + " Row " + String.valueOf(rowNumber) + ": form mode"),
-                new ScrollableView(context, new GrowToFit(new Column(buildGrid(),
-                        new GrowToFit())),
+        return new Column(
+                new Header(context,
+                        tableName + " Row " + String.valueOf(rowNumber + 1) + ": form mode"),
+                new ScrollableView(context, new GrowToFit(new Column(buildGrid(), new GrowToFit())),
                         scrollController)).setCrossAxisAlignment(Alignment.STRETCH);
     }
 
@@ -114,24 +120,33 @@ public class TableFormsView extends DatabaseScreen implements TableDataChangeLis
         ArrayList<ArrayList<String>> rows = context.getDatabase().getRows(tableName);
         List<String> columnNames = context.getDatabase().getColumnNames(tableName);
 
+        items.addAll(List.of(
+                // Column name
+                new Padding(new Text("Column Name")).all(2),
+                new Padding(new Text("Cell Value")).all(2)));
+
         if (rowNumber < rows.size()) {
             List<String> row = rows.get(rowNumber);
             int size = Math.min(row.size(), columnNames.size());
             for (int i = 0; i < size; i++) {
-            String columnValue = row.get(i);
-            String columnName = columnNames.get(i);
+                String columnValue = row.get(i);
+                String columnName = columnNames.get(i);
 
-            items.addAll(List.of(
-                    // Column name
-                    new InputText(context, columnValue, text -> {
-                        context.getDatabase().updateCell(tableName, columnName, rowNumber, text);
-                    }).setValidationFunction(
-                            name -> (context.getDatabase().isValidValue(tableName, columnName, name)))
-                    ));
+                items.addAll(List.of(
+                        // Column name
+                        new Padding(new Text(columnName)).all(5),
+
+                        new Padding(new InputText(context, columnValue, text -> {
+                            context.getDatabase().updateCell(tableName, columnName, rowNumber,
+                                    text);
+                        }).setValidationFunction(name -> (context.getDatabase()
+                                .isValidValue(tableName, columnName, name)))).all(5)
+
+                ));
             }
         }
-        
-        return new ResizableGrid(context, new ResizableGridController(context, 1, 5), items);
+
+        return new Grid(2, Orientation.VERTICAL, false, items);
     }
 
     @Override
